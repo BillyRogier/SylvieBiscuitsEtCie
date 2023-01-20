d770dc04ca6e1b62a068a6e904e8107f97375d28
Billy Rogier <72304499+BillyRogier@users.noreply.github.com>
2023-01-20 11:28:28 +0100

    Update contact_form.php

--- contact_form.php
+++ contact_form.php
@@ -3,7 +3,7 @@ if (isset($_POST)) {
     $name = $_REQUEST['name'];
     $email = $_REQUEST['email'];
     $message = $_REQUEST['message'];
-    $to = 'sylviebiscuitsetcie@sbc.fr';
+    $to = 'email';
     $subject = 'Contact Request From Website';
     $headers = "From: " . $name . " <" . $email . "> \r\n";
     $send_email = mail($to, $subject, $message, $headers);
